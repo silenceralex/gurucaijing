d64a2e277916c31a5e015baf44a74f690c476357
shadowcj <shadowcj@910414aa-d3c9-269b-9558-6cae95c76e5e>
2010-07-03 06:44:11 +0000

    添加数字签名jar
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@9 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/mail/PDFReader.java
+++ caijing/src/main/java/com/caijing/mail/PDFReader.java
@@ -2,6 +2,7 @@ package com.caijing.mail;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.MalformedURLException;
@@ -26,7 +27,7 @@ public class PDFReader {
 		}
 	}
 
-	public void readFdf(String file) throws Exception {
+	public void readFdf(String file) {
 		// 是否排序
 		boolean sort = false;
 		// pdf文件名
@@ -44,27 +45,20 @@ public class PDFReader {
 		// 内存中存储的PDF Document
 		PDDocument document = null;
 		try {
-			try {
-				// 首先当作一个URL来装载文件，如果得到异常再从本地文件系统//去装载文件
-				URL url = new URL(pdfFile); // 注意参数已不是以前版本中的URL.而是File。
-				document = PDDocument.load(pdfFile);
-				// 获取PDF的文件名
-				String fileName = url.getFile();
-				// 以原来PDF的名称来命名新产生的txt文件
-				if (fileName.length() > 4) {
-					File outputFile = new File(fileName.substring(0, fileName
-							.length() - 4)
-							+ ".txt");
-					textFile = outputFile.getName();
-				}
-			} catch (MalformedURLException e) {
-				// 如果作为URL装载得到异常则从文件系统装载 //注意参数已不是以前版本中的URL.而是File。
-				document = PDDocument.load(pdfFile);
-				if (pdfFile.length() > 4) {
-					textFile = pdfFile.substring(0, pdfFile.length() - 4)
-							+ ".txt";
-				}
+			// 首先当作一个URL来装载文件，如果得到异常再从本地文件系统//去装载文件
+//			URL url = new URL(pdfFile); // 注意参数已不是以前版本中的URL.而是File。
+			document = PDDocument.load(pdfFile);
+			// 获取PDF的文件名
+//			String fileName = url.getFile();
+			String fileName = pdfFile;
+			// 以原来PDF的名称来命名新产生的txt文件
+			if (fileName.length() > 4) {
+				File outputFile = new File(fileName.substring(0, fileName
+						.length() - 4)
+						+ ".txt");
+				textFile = outputFile.getName();
 			}
+		
 			// 文件输入流，写入文件倒textFile
 			output = new OutputStreamWriter(new FileOutputStream(textFile),
 					encoding);
@@ -79,14 +73,15 @@ public class PDFReader {
 			stripper.setEndPage(endPage);
 			// 调用PDFTextStripper的writeText提取并输出文本
 			stripper.writeText(document, output);
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
 		} finally {
-			if (output != null) {
-				// 关闭输出流
+			try {
 				output.close();
-			}
-			if (document != null) {
-				// 关闭PDF Document
 				document.close();
+			} catch (Exception e) {
+				System.out.print(e.getMessage());
+//				e.printStackTrace();
 			}
 		}
 	}
@@ -99,7 +94,8 @@ public class PDFReader {
 		PDFReader pdfReader = new PDFReader();
 		try {
 			// 取得E盘下的SpringGuide.pdf的内容
-			pdfReader.readFdf("/home/email/papers");
+			pdfReader.read("/home/email/papers");
+			// pdfReader.readFdf("/home/email/papers/20100608/zx.pdf");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
