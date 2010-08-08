e63c310a1e020385b0ec5520ecf697baaa058669
shadowcj <shadowcj@910414aa-d3c9-269b-9558-6cae95c76e5e>
2010-08-08 17:03:04 +0000

    加入安信证券的解析
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@64 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/mail/MailSchedule.java
+++ caijing/src/main/java/com/caijing/mail/MailSchedule.java
@@ -4,15 +4,14 @@ public class MailSchedule {
 
 	public void processMail() {
 		MailReceiver receiver = new MailReceiver();
-//		receiver.setHost("pop.126.com");
 		receiver.setUsername("bg20052008");// 您的邮箱账号
 		receiver.setPassword("336699");// 您的邮箱密码
-		// receiver.setAttachPath("f:\\email");//您要存放附件在什么位置？绝对路径
 		receiver.setAttachPath("/home/app/email");// 您要存放附件在什么位置？绝对路径
 		try {
 			receiver.reveiveMail();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("process over!");
 	}
 }
