564d10d20aff30a0073e9e765a7270d10c92490a
shadowcj <shadowcj@910414aa-d3c9-269b-9558-6cae95c76e5e>
2010-09-06 14:16:38 +0000

    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@134 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/spide/test.java
+++ caijing/src/main/java/com/caijing/spide/test.java
@@ -15,40 +15,18 @@ public class test {
 	/**
 	 * @param args
 	 * @throws IOException 
-	 * @throws HttpException 
+	 * @throws HttpException  * 
+	 * 
 	 */
+	
+	
+	
+	public static void hello(){
+		System.out.println(test.class.getResource(""));
+	}
+	
 	public static void main(String[] args) throws HttpException, IOException {
-		// TODO Auto-generated method stub
-//		String ying="Ӣabc";
-//		char tmp = ying.charAt(1);
-//		System.out.println(tmp);		
-//		String   yingHex   =   Integer.toHexString(tmp); 
-//		System.out.println(yingHex);
 		
-//		String ying="Ӣ";
-//		byte[] byteGB2312 = ying.getBytes("GB2312");
-//		byte[] byteDefault = ying.getBytes();
-//		String newStrGB = new String(byteGB2312, "GB2312");
-//		String newStrDefault = new String(byteDefault);
-//		
-//		System.out.println(newStrGB);
-//		System.out.println(newStrDefault);
-		
-		HttpClient client = new HttpClient();
-		GetMethod authget = new GetMethod("http://blog.sina.com.cn/s/blog_49a3971d0100kyan.html");
-		client.executeMethod(authget);
-		InputStream input = authget.getResponseBodyAsStream();
-		InputStreamReader re = new InputStreamReader(input, "UTF-8");
-		BufferedReader br = new BufferedReader(re);
-		String s,content="";
-		s=br.readLine();
-		while(s != null){
-			content+=s;
-		    s=br.readLine();		    
-		}
-		br.close();
-		System.out.println(content);
-
 	}
 
 }
