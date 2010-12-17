1fd2e749e3277cf4c62fd937e9ad9e57c72f8165
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2010-12-17 11:32:32 +0000

    update
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@382 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
+++ caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
@@ -611,10 +611,13 @@ public class ReportExtractorImpl implements ReportExtractor {
 		//公司研报
 		Report report = new Report();
 		report.setSaname("华泰联合");
-		//extractor.extractFromFile(report, "http://www.51gurus.com/papers/20101102/6JJABJV9.txt");
-		//extractor.extractFromFile(report, "http://www.51gurus.com/papers/20101207/6ME38N57.txt");
+		extractor.extractFromFile(report, "http://www.51gurus.com/papers/20101102/6JJABJV9.txt");
+		extractor.extractFromFile(report, "http://www.51gurus.com/papers/20101207/6ME38N57.txt");
 		extractor.extractFromFile(report, "http://www.51gurus.com/papers/20101207/6ME38PU2.txt");
-
+		extractor.extractFromFile(report, "http://51gurus.com/papers/20100916/6FQ6LHOR.txt");
+		extractor.extractFromFile(report, "http://51gurus.com/papers/20101026/6JG010EI.txt");
+		
+		
 	}
 
 	public Config getConfig() {
