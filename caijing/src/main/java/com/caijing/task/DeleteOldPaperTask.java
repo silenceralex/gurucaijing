f23431101ec2ad8cc4c34125c7b201258b4c2ef4
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-01-21 11:02:38 +0000

    添加历史研报清楚任务
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@497 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/task/DeleteOldPaperTask.java
+++ caijing/src/main/java/com/caijing/task/DeleteOldPaperTask.java
@@ -20,21 +20,25 @@ public class DeleteOldPaperTask {
 	String deletereportsql = "delete from report where rid=?";
 	String deleterecommendstocksql = "delete from recommendstock where reportid=?";
 	String deleteoldhtml = "sh /data/shells/rm.sh";
-	
+
 	public void run() {
+		System.out.println("==task start==");
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) ContextFactory.getBean("jdbcTemplate");
 		List<Map<String, String>> rows = jdbcTemplate.queryForList(queryreportsql);
-		for (Map<String, String> row : rows) {
-			String rid = (String) row.get("rid");
-			System.out.println(rid);
-			jdbcTemplate.update(deletereportsql,new Object[]{rid});
-			jdbcTemplate.update(deleterecommendstocksql,new Object[]{rid});
-			StringWriter sw = new StringWriter();
-			Command.run(deleteoldhtml, sw);
-			System.out.println(sw.toString());
+		if (rows != null && rows.size() > 0) {
+			for (Map<String, String> row : rows) {
+				String rid = (String) row.get("rid");
+				System.out.print(rid);
+				System.out.print(" " + jdbcTemplate.update(deletereportsql, new Object[] { rid }));
+				System.out.println(" " + jdbcTemplate.update(deleterecommendstocksql, new Object[] { rid }));
+				StringWriter sw = new StringWriter();
+				Command.run(deleteoldhtml, sw);
+				System.out.println(sw.toString());
+			}
 		}
+		System.out.println("==task exit==");
 	}
-	
+
 	public static void main(String[] args) {
 		DeleteOldPaperTask task = new DeleteOldPaperTask();
 		task.run();
