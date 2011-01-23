19c9ef1bb3aa1f344f19bd4d63599420747a251b
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-01-23 19:03:35 +0000

    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@549 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
+++ caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
@@ -495,6 +495,7 @@ public class ReportExtractorImpl implements ReportExtractor {
 				System.out.println("aname:" + aname);
 			} else {
 				System.out.println("No match:");
+				FileUtil.appendWrite(invalidoldpapers+"gjzq"+".log", path+"\n", "UTF-8");
 			}
 		} else if (saname.equalsIgnoreCase("¹úÐÅÖ¤È¯")) {
 			if (m != null && m.find()) {
