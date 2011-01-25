c81babb20db29f9ac733576219435c5e3c92c079
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-01-25 16:15:17 +0000

    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@604 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/mail/PDFReader.java
+++ caijing/src/main/java/com/caijing/mail/PDFReader.java
@@ -249,10 +249,10 @@ public class PDFReader {
 //				processOneFile(line, "长江证券");
 //				count++;
 //			}
-//			if (line.contains("光大：")) {//没有研报，暂不处理
-//				processOneFile(line, "光大证券");
-//				count++;
-//			}
+			if (line.contains("光大：")) {//没有研报，暂不处理
+				processOneFile(line, "光大证券");
+				count++;
+			}
 //			if (line.contains("东海：")) {//目前没有这个券商，暂不处理
 //				processOneFile(line, "东海证券");
 //				count++;
