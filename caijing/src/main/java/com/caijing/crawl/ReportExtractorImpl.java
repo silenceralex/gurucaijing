2c7e77990beba684579a717cbd7468f8a4f7aaae
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-01-27 03:23:49 +0000

    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@625 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
+++ caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
@@ -355,7 +355,7 @@ public class ReportExtractorImpl implements ReportExtractor {
 		System.out.println("path:" + path);
 		
 		List<String> titlePatterns = (List<String>) config.getValue(saname).get("titlePattern");
-		if(titlePatterns!=null && saname.equalsIgnoreCase("安信证券")){
+		if(titlePatterns!=null && saname.equalsIgnoreCase("申银万国")){
 			for (String pattern : titlePatterns) {
 				int i = 1;
 				Pattern titlePattern = Pattern.compile(pattern,	Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.UNIX_LINES);
@@ -363,34 +363,36 @@ public class ReportExtractorImpl implements ReportExtractor {
 				Matcher m = titlePattern.matcher(path);
 				
 				if (i++==1 && m != null && m.find()) {
-					String sanam = m.group(1);
-					String stockname = m.group(2);
-					String title = m.group(3);
-					String stockcode = m.group(4);
+					String sanam = m.group(1); //saname
+					String stockname = m.group(4);
+					String title = m.group(6);
+					String stockcode = m.group(3);
 					String aname = m.group(5);
 					report.setSaname(sanam);
 					report.setStockcode(stockcode);
 					report.setStockname(stockname);
-					report.setType(1);
+					report.setType(1); //m.group(2)
 					report.setTitle(title);
 					report.setAname(aname);
+					//TODO 将title抽取出来的字段写入一个日志文件，每个券商一个日志
 					System.out.println("sanam:" + sanam);
 					System.out.println("stockname(code):" + stockname + "(" + stockcode + ")");
 					System.out.println("type:" + 1);
 					System.out.println("title:" + title);
 					System.out.println("aname:" + aname);
 				} if (i++==2 && m != null && m.find()) {
-					String sanam = m.group(1);
-					String stockname = m.group(2);
-					String title = m.group(3);
-					String stockcode = m.group(4);
-					String aname = m.group(5);
+					String sanam = m.group(1); //saname
+					String stockname = m.group(4);
+					String title = m.group(5);
+					String stockcode = m.group(3);
+					String aname = null;
 					report.setSaname(sanam);
 					report.setStockcode(stockcode);
 					report.setStockname(stockname);
-					report.setType(1);
+					report.setType(1); //m.group(2)
 					report.setTitle(title);
 					report.setAname(aname);
+					//TODO 将title抽取出来的字段写入一个日志文件，每个券商一个日志
 					System.out.println("sanam:" + sanam);
 					System.out.println("stockname(code):" + stockname + "(" + stockcode + ")");
 					System.out.println("type:" + 1);
