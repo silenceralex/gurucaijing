30bdbd82e0978508931a610fe8eac26050ee1759
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-01-29 15:14:43 +0000

    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@666 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
+++ caijing/src/main/java/com/caijing/crawl/ReportExtractorImpl.java
@@ -825,7 +825,51 @@ public class ReportExtractorImpl implements ReportExtractor {
 				System.out.println("No match:");
 				FileUtil.appendWrite(invalidoldpapers + "htzq" + ".log", path + "\n", "UTF-8");
 			}
-		}			
+		}		
+
+		if (titlePatterns != null && saname.equalsIgnoreCase("中金公司")) {
+			for (String pattern : titlePatterns) {
+				i++;
+				Pattern titlePattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.UNIX_LINES);
+				Matcher m = titlePattern.matcher(path);
+				if (i == 1 && m != null && m.find()) {
+					System.out.println("titlePattern:" + titlePattern);
+					String sanam = saname;
+					String stockname = m.group(1); 
+					String title = m.group(3);
+					String stockcode = m.group(2);
+					String aname = null;
+					setReport(report, sanam, stockname, stockcode, aname, title, 1);
+					i=-1;
+					break;
+				} else if (i == 2 && m != null && m.find()) {
+					System.out.println("titlePattern:" + titlePattern);
+					String sanam = saname;
+					String stockname = m.group(1); 
+					String title = m.group(2);
+					String stockcode = null;
+					String aname = null;
+					setReport(report, sanam, stockname, stockcode, aname, title, 1);
+					i=-1;
+					break;						
+				} else if (i == 3 && m != null && m.find()) {
+					System.out.println("titlePattern:" + titlePattern);
+					String sanam = saname;
+					String stockname = m.group(1); 
+					String title = m.group(2);
+					String stockcode = null;
+					String aname = null;
+					setReport(report, sanam, stockname, stockcode, aname, title, 1);
+					i=-1;
+					break;
+				}
+			}
+			if (i != -1) {
+				System.out.println("No match:");
+				FileUtil.appendWrite(invalidoldpapers + "htzq" + ".log", path + "\n", "UTF-8");
+			}
+		}	
+		
 		/*
 		Pattern titlePattern = Pattern.compile((String) config.getValue(saname).get("titlePattern"),
 				Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.UNIX_LINES);
