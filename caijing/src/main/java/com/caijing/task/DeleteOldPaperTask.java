b42bc03981bc9b18c7eabab6f7f913b21f678da2
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-02-15 13:27:17 +0000

    添加删除单个券商的历史研报方法
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@759 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/task/DeleteOldPaperTask.java
+++ caijing/src/main/java/com/caijing/task/DeleteOldPaperTask.java
@@ -14,13 +14,13 @@ import com.caijing.util.ContextFactory;
  */
 public class DeleteOldPaperTask {
 
-	String queryreportsql = "select rid from report where filepath like \"/home/oldhtml/papers/%\"";
-	//String queryreportsql = "select rid from report where filepath like \"/home/oldhtml/papers/%\" and ptime>\"2011-01-28 9:30:00\"";
-	String deletereportsql = "delete from report where rid=?";
-	String deleterecommendstocksql = "delete from recommendstock where reportid=?";
-	String deleteoldhtml = "sh /data/shells/rm.sh";
-
-	public void run() {
+	public void delAllOldReports() {
+		String queryreportsql = "select rid from report where filepath like \"/home/oldhtml/papers/%\"";
+		//String queryreportsql = "select rid from report where filepath like \"/home/oldhtml/papers/%\" and ptime>\"2011-01-28 9:30:00\"";
+		String deletereportsql = "delete from report where rid=?";
+		String deleterecommendstocksql = "delete from recommendstock where reportid=?";
+		String deleteoldhtml = "sh /data/shells/rm.sh";
+		
 		System.out.println("==task start==");
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) ContextFactory.getBean("jdbcTemplate");
 		List<Map<String, String>> rows = jdbcTemplate.queryForList(queryreportsql);
@@ -38,9 +38,33 @@ public class DeleteOldPaperTask {
 		System.out.println("==task exit==");
 	}
 
+	public void delSpecialSanameOldReports(String saname){
+		String queryreportsql = "select rid from report where filepath like \"/home/oldhtml/papers/%\" and saname=?";
+		String deletereportsql = "delete from report where rid=?";
+		String deleterecommendstocksql = "delete from recommendstock where reportid=?";
+		String deleteoldhtml = "rm /home/oldhtml/";
+		
+		System.out.println("==delSpecialSaname task start==");
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) ContextFactory.getBean("jdbcTemplate");
+		List<Map<String, String>> rows = jdbcTemplate.queryForList(queryreportsql, new Object[] { saname });
+		if (rows != null && rows.size() > 0) {
+			for (Map<String, String> row : rows) {
+				String rid = (String) row.get("rid");
+				System.out.print(rid);
+				System.out.print(" " + jdbcTemplate.update(deleterecommendstocksql, new Object[] { rid }));
+				System.out.print(" " + jdbcTemplate.update(deletereportsql, new Object[] { rid }));
+				StringWriter sw = new StringWriter();
+				deleteoldhtml = deleteoldhtml+rid+".pdf";
+				Command.run(deleteoldhtml, sw);
+				System.out.println(" "+sw.toString());
+			}
+		}
+		System.out.println("==delSpecialSaname task exit==");
+	}
+	
 	public static void main(String[] args) {
 		DeleteOldPaperTask task = new DeleteOldPaperTask();
-		task.run();
+		//task.delAllOldReports();
 		System.exit(0);
 	}
 }
