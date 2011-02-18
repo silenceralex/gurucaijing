c169a5fd6577e3eb75c7ceda293b415b52169f4f
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-02-18 15:40:35 +0000

    股票代码映射
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@771 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/task/Stockcode2StocknameTask.java
+++ caijing/src/main/java/com/caijing/task/Stockcode2StocknameTask.java
@@ -2,6 +2,8 @@ package com.caijing.task;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -14,26 +16,34 @@ import com.caijing.util.ContextFactory;
  */
 public class Stockcode2StocknameTask {
 
+	private Pattern stockcodePattern = Pattern.compile("(((002|000|300|600)[\\d]{3})|60[\\d]{4})",
+			Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.UNIX_LINES);	
+	
 	String querystocksql = "select stockcode,stockname from stock";
-	String queryreportsql = "select rid,stockcode,stockname,title from report where filepath like \"/home/oldhtml/papers/%\" limit 10";
+	//String queryreportsql = "select rid,stockcode,stockname,title from report where filepath like \"/home/oldhtml/papers/%\" limit 10";
 	String updatereportsql = "update report set stockcode=?,stockname=? where rid=?";
 
 	String deletereportsql = "delete from report where rid=?";
 	String deleterecommendstocksql = "delete from recommendstock where reportid=?";
+	
+	String queryreportsql = "select rid,stockcode,stockname,title,filepath from report where filepath like \"/oldhtml/papers/%\" and (stockcode=? or stockname=?) limit 10";
 
+	
+	@SuppressWarnings("unchecked")
 	public void run() {
 		System.out.println("==task start==");
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) ContextFactory.getBean("jdbcTemplate");
 
 		List<Map<String, String>> stocks = jdbcTemplate.queryForList(querystocksql);
-		List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql);
+		//List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql);
 
 		if (stocks != null && stocks.size() > 0) {
 			for (Map<String, String> stock : stocks) {
 				String stockcode = stock.get("stockcode");
 				String stockname = stock.get("stockname");
 				System.out.println("stock: " + stockcode + " " + stockname);
-
+				List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql, new Object[]{stockcode, stockname});
+				
 				if (reports != null && reports.size() > 0) {
 					for (Map<String, String> report : reports) {
 						String rid = report.get("rid");
@@ -41,20 +51,27 @@ public class Stockcode2StocknameTask {
 						String name = report.get("stockname");
 						String title = report.get("title");
 						//System.out.println(rid + " " + code + " " + name);
+						
+						boolean isnotcode = (code == null || code.trim().length() == 0);
+						if (!isnotcode){
+							Matcher m = stockcodePattern.matcher(code);
+							if (m != null && m.find()) {
+								isnotcode = false;
+							}
+						}
 
-						boolean iscode = (code == null || code.trim().length() == 0);
-						boolean isname = (name == null || name.trim().length() == 0);
-						boolean istitle = (title == null || title.trim().length() == 0);
+						boolean isnotname = (name == null || name.trim().length() == 0);
+						boolean isnottitle = (title == null || title.trim().length() == 0);
 
-						if (iscode && !isname) {
+						if (isnotcode && !isnotname) {
 							//stockcode为空，stockname不为空
 							System.out.println("update " + rid + "(" + "!" + stockcode + "," + name + ")" + title);
 							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
-						} else if (!iscode && isname) {
+						} else if (!isnotcode && isnotname) {
 							//stockcode不为空，stockname为空
 							System.out.println("update " + rid + "(" + code + "," + "!" + stockname + ")" + title);
 							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
-						} else if (iscode && isname && istitle) {
+						} else if (isnotcode && isnotname && isnottitle) {
 							System.out.println("delete " + rid + "(" + code + "," + name + ")" + title);
 							//System.out.print(" " + jdbcTemplate.update(deleterecommendstocksql, new Object[] { rid }));
 							//System.out.print(" " + jdbcTemplate.update(deletereportsql, new Object[] { rid }));
