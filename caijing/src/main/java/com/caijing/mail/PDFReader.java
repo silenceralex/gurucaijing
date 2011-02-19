b38eabf645d58ed3662bce272c0f13ea8fe7e9f5
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-02-19 16:10:21 +0000

    历史研报提取激活
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@779 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/mail/PDFReader.java
+++ caijing/src/main/java/com/caijing/mail/PDFReader.java
@@ -204,7 +204,7 @@ public class PDFReader {
 		//		int start = 1; //开始的行号
 		//		int end = 3693; //最后的行号
 		//		int i = 0; //当前的行号
-		int limit = 300000;
+		int limit = 300000000;
 		int count = 0;
 		
 		List<String> processList = new ArrayList<String>();
@@ -223,70 +223,70 @@ public class PDFReader {
 			processList.add(line);
 			
 			
-			if (count == limit) {
-				break;
-			}
+//			if (count == limit) {
+//				break;
+//			}
 			//			http://51gurus.com/oldhtml/papers/200801-200903/6Q8N412F.pdf
-//			if (line.contains("申银万国_")) {//1-3693
-//				processOneFile(line, "申银万国");
-//				count++;
-//			}
-//			if (line.contains("招商证券")) {//3694-5077
-//				processOneFile(line, "招商证券");
-//				count++;
-//			}
-//			if (line.contains("国泰君安")) {//5078-8316
-//				processOneFile(line, "国泰君安");
-//				count++;
-//			}
-//			if (line.contains("安信证券")) {//5078-8316
-//				processOneFile(line, "安信证券");
-//				count++;
-//			}
-//			if (line.contains("广发：") || line.contains("广发证券")) {
-//				processOneFile(line, "广发证券");
-//				count++;
-//			}
-//			if (line.contains("国金证券")) {//8484-8879
-//				processOneFile(line, "国金证券");
-//				count++;
-//			}
-//			if (line.contains("国信证券")) {//没有stockcode
-//				processOneFile(line, "国信证券");
-//				count++;
-//			}
-//			if (line.contains("长江：")) {//数量很少，基本只有title
-//				processOneFile(line, "长江证券");
-//				count++;
-//			}
-//			if (line.contains("光大：")) {
-//				processOneFile(line, "光大证券");
-//				count++;
-//			}
+			if (line.contains("申银万国_")) {//1-3693
+				processOneFile(line, "申银万国");
+				count++;
+			}
+			if (line.contains("招商证券")) {//3694-5077
+				processOneFile(line, "招商证券");
+				count++;
+			}
+			if (line.contains("国泰君安")) {//5078-8316
+				processOneFile(line, "国泰君安");
+				count++;
+			}
+			if (line.contains("安信证券")) {//5078-8316
+				processOneFile(line, "安信证券");
+				count++;
+			}
+			if (line.contains("广发：") || line.contains("广发证券")) {
+				processOneFile(line, "广发证券");
+				count++;
+			}
+			if (line.contains("国金证券")) {//8484-8879
+				processOneFile(line, "国金证券");
+				count++;
+			}
+			if (line.contains("国信证券")) {//没有stockcode
+				processOneFile(line, "国信证券");
+				count++;
+			}
+			if (line.contains("长江：")) {//数量很少，基本只有title
+				processOneFile(line, "长江证券");
+				count++;
+			}
+			if (line.contains("光大：")) {
+				processOneFile(line, "光大证券");
+				count++;
+			}
 			//			if (line.contains("东海：")) {//目前没有这个券商，暂不处理
 			//				processOneFile(line, "东海证券");
 			//				count++;
 			//			}
-//			if (line.contains("华泰：")) {//编码问题，导致有的研报不能读取
-//				processOneFile(line, "华泰证券");
-//				count++;
-//			}
-//			if (line.contains("联合：")) {//编码问题，导致有的研报不能读取
-//				processOneFile(line, "华泰联合");
-//				count++;
-//			}
-//			if (line.contains("中金：")) {
-//				processOneFile(line, "中金公司");
-//				count++;
-//			}
-//			if (line.contains("中投：")) {
-//				processOneFile(line, "中投证券");
-//				count++;
-//			}
-//			if (line.contains("中信建投")) {
-//				processOneFile(line, "中信建投");
-//				count++;
-//			}
+			if (line.contains("华泰：")) {//编码问题，导致有的研报不能读取
+				processOneFile(line, "华泰证券");
+				count++;
+			}
+			if (line.contains("联合：")) {//编码问题，导致有的研报不能读取
+				processOneFile(line, "华泰联合");
+				count++;
+			}
+			if (line.contains("中金：")) {
+				processOneFile(line, "中金公司");
+				count++;
+			}
+			if (line.contains("中投：")) {
+				processOneFile(line, "中投证券");
+				count++;
+			}
+			if (line.contains("中信建投")) {
+				processOneFile(line, "中信建投");
+				count++;
+			}
 
 			/*
 			if (logfile.contains("axzq")) {
