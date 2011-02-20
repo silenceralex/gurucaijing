ec5c35aba622c2ed285a2b39c8b2e0848774529a
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-02-20 13:47:59 +0000

    股票代码映射
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@780 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/task/Stockcode2StocknameTask.java
+++ caijing/src/main/java/com/caijing/task/Stockcode2StocknameTask.java
@@ -21,13 +21,13 @@ public class Stockcode2StocknameTask {
 	private static String Prifix = "/oldhtml/papers/";
 	
 	String querystocksql = "select stockcode,stockname from stock";
-	//String queryreportsql = "select rid,stockcode,stockname,title from report where filepath like \"/home/oldhtml/papers/%\" limit 10";
+	String queryreportsql = "select rid,stockcode,stockname,title from report where filepath like \""+Prifix+"%\" limit 1000";
 	String updatereportsql = "update report set stockcode=?,stockname=? where rid=?";
 
 	String deletereportsql = "delete from report where rid=?";
 	String deleterecommendstocksql = "delete from recommendstock where reportid=?";
 	
-	String queryreportsql = "select rid,stockcode,stockname,title,filepath from report where filepath like \""+Prifix+"%\" and (stockcode=? or stockname=?)";
+	//String queryreportsql = "select rid,stockcode,stockname,title,filepath from report where filepath like \""+Prifix+"%\" and (stockcode=? or stockname=?) limit 10";
 
 	@SuppressWarnings("unchecked")
 	public void run() {
@@ -35,48 +35,76 @@ public class Stockcode2StocknameTask {
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) ContextFactory.getBean("jdbcTemplate");
 
 		List<Map<String, String>> stocks = jdbcTemplate.queryForList(querystocksql);
-		//List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql);
+		List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql);
 
 		if (stocks != null && stocks.size() > 0) {
 			for (Map<String, String> stock : stocks) {
-				String stockcode = stock.get("stockcode");
-				String stockname = stock.get("stockname");
+				String stockcode = stock.get("stockcode").trim();
+				String stockname = stock.get("stockname").trim();
 				System.out.println("stock: " + stockcode + " " + stockname);
-				List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql, new Object[]{stockcode, stockname});
+				
+				//List<Map<String, String>> reports = jdbcTemplate.queryForList(queryreportsql, new Object[]{stockcode, stockname});
 				
 				if (reports != null && reports.size() > 0) {
 					for (Map<String, String> report : reports) {
 						String rid = report.get("rid");
-						String code = report.get("stockcode");
-						String name = report.get("stockname");
-						String title = report.get("title");
-						//System.out.println(rid + " " + code + " " + name);
+						String code = report.get("stockcode")==null?null:report.get("stockcode").trim();
+						String name = report.get("stockname")==null?null:report.get("stockname").trim();
+						String title = report.get("title")==null?null:report.get("title").trim();
+						System.out.println(rid + " " + code + " " + name);
 						
-						boolean isnotcode = (code == null || code.trim().length() == 0);
-						if (!isnotcode){
-							Matcher m = stockcodePattern.matcher(code);
-							if (m != null && m.find()) {
-								isnotcode = false;
-							}
-						}
-
-						boolean isnotname = (name == null || name.trim().length() == 0);
-						boolean isnottitle = (title == null || title.trim().length() == 0);
-
-						if (isnotcode && !isnotname) {
-							//stockcode为空，stockname不为空
+						boolean equalcode = stockcode.equals(code);
+						boolean equalname = stockname.equals(name);
+						
+						if(equalcode && equalname){
+							reports.remove(report);
+							continue;
+						} else if(!equalcode && equalname){
+							//stockcode不合法，stockname合法
 							System.out.println("update " + rid + "(" + "!" + stockcode + "," + name + ")" + title);
 							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
-						} else if (!isnotcode && isnotname) {
-							//stockcode不为空，stockname为空
+							reports.remove(report);
+							continue;
+						} else if(equalcode && !equalname){
+							//stockcode合法，stockname不合法
 							System.out.println("update " + rid + "(" + code + "," + "!" + stockname + ")" + title);
 							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
-						} else if (isnotcode && isnotname && isnottitle) {
-							System.out.println("delete " + rid + "(" + code + "," + name + ")" + title);
-							//System.out.print(" " + jdbcTemplate.update(deleterecommendstocksql, new Object[] { rid }));
-							//System.out.print(" " + jdbcTemplate.update(deletereportsql, new Object[] { rid }));
-						}
+							reports.remove(report);
+							continue;
+						} 
+						
+//						boolean isvalidcode = !(code == null || code.length() == 0);
+//						if (isvalidcode){
+//							Matcher m = stockcodePattern.matcher(code);
+//							if (m != null && m.find()) {
+//								isvalidcode = true;
+//							}
+//						}
+//						boolean isvalidname = !(name == null || name.length() == 0);
+//						boolean isvalidtitle = !(title == null || title.length() == 0);
+//						if (!isvalidcode && isvalidname) {
+//							//stockcode为空，stockname不为空
+//							System.out.println("update " + rid + "(" + "!" + stockcode + "," + name + ")" + title);
+//							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
+//						} else if (isvalidcode && !isvalidname) {
+//							//stockcode不为空，stockname为空
+//							System.out.println("update " + rid + "(" + code + "," + "!" + stockname + ")" + title);
+//							//System.out.println("update " + rid +jdbcTemplate.update(updatereportsql, new Object[] { stockcode, stockname, rid }));
+//						} else if (!isvalidcode && !isvalidname && !isvalidtitle) {
+//							System.out.println("delete " + rid + "(" + code + "," + name + ")" + title);
+//							//System.out.print(" " + jdbcTemplate.update(deleterecommendstocksql, new Object[] { rid }));
+//							//System.out.print(" " + jdbcTemplate.update(deletereportsql, new Object[] { rid }));
+//						}
 
+					}
+					
+					System.out.println("无法处理的历史研报: ");
+					for (Map<String, String> report : reports) {
+						String rid = report.get("rid");
+						String code = report.get("stockcode").trim();
+						String name = report.get("stockname").trim();
+						String title = report.get("title").trim();
+						System.out.println(rid + " " + code + " " + name+" "+ title);
 					}
 				}
 			}
@@ -88,6 +116,7 @@ public class Stockcode2StocknameTask {
 	public static void main(String[] args) {
 		Stockcode2StocknameTask task = new Stockcode2StocknameTask();
 		task.run();
+		//System.out.println("aa".equals(null));
 		System.exit(0);
 	}
 
