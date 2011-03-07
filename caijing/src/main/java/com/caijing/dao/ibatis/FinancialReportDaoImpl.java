6fb808f04a791fdba107ee709ae4e4164cf83162
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-03-07 09:45:00 +0000

    财报lab
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@922 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/dao/ibatis/FinancialReportDaoImpl.java
+++ caijing/src/main/java/com/caijing/dao/ibatis/FinancialReportDaoImpl.java
@@ -1,17 +1,22 @@
 package com.caijing.dao.ibatis;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.caijing.dao.FinancialReportDao;
-import com.caijing.domain.Report;
+import com.caijing.domain.FinancialReport;
 import com.caijing.util.CrudDaoDefault;
 
 public class FinancialReportDaoImpl extends CrudDaoDefault implements FinancialReportDao {
 
 	@Override
-	public List<Report> getReportsListByStatus(int status, int start, int size) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<FinancialReport> getReportsListByStatus(int status, int start, int size) {
+		Map<String, Object> params = new HashMap<String, Object>(3);
+		params.put("status", status);
+		params.put("offset", start);
+		params.put("length", size);
+		return (List<FinancialReport>) getSqlMapClientTemplate().queryForList(this.getNameSpace() + ".getReportsListByStatus", params);
 	}
 	
 	
