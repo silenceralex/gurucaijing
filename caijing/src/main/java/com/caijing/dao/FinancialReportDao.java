0ceffc7ea8acfb7c6c8791bd97ce8c570c7cf49e
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-03-07 09:16:09 +0000

    财报lab
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@921 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/dao/FinancialReportDao.java
+++ caijing/src/main/java/com/caijing/dao/FinancialReportDao.java
@@ -1,7 +1,12 @@
 package com.caijing.dao;
 
+import java.util.List;
+
+import com.caijing.domain.Report;
 import com.caijing.util.CrudDao;
 
 public interface FinancialReportDao extends CrudDao {
+
+	List<Report> getReportsListByStatus(int status, int start, int size);
 
 }
--- caijing/src/main/java/com/caijing/dao/ibatis/FinancialReportDaoImpl.java
+++ caijing/src/main/java/com/caijing/dao/ibatis/FinancialReportDaoImpl.java
@@ -1,9 +1,18 @@
 package com.caijing.dao.ibatis;
 
+import java.util.List;
+
 import com.caijing.dao.FinancialReportDao;
+import com.caijing.domain.Report;
 import com.caijing.util.CrudDaoDefault;
 
 public class FinancialReportDaoImpl extends CrudDaoDefault implements FinancialReportDao {
+
+	@Override
+	public List<Report> getReportsListByStatus(int status, int start, int size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 }
