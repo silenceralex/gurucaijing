4abddb84c73e7e8ea165d69e91152415e2c6414d
shadowcj <shadowcj@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-09 15:31:33 +0000

    解决抓取不完整的问题
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1099 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/crawl/ListExtractor.java
+++ caijing/src/main/java/com/caijing/crawl/ListExtractor.java
@@ -50,86 +50,86 @@ public class ListExtractor implements Extractor {
 	private Config config = null;
 
 	public Post extractFromHtml(String content, String groupID) {
-		Matcher m = rangePattern.matcher(content);
+		//		Matcher m = rangePattern.matcher(content);
 		Post post = new Post();
-		if (m != null && m.find()) {
-			String inner = m.group(1);
-			//				System.out.println("inner: " + inner);
-
-			Matcher m4 = nickPattern.matcher(inner);
-			if (m4 != null && m4.find()) {
-				String nick = m4.group(1).replaceAll("\\s", "").replaceAll("<.*?>", "").trim();
-				post.setNick(nick);
-				//				if (!nick.equals(config.getValue("groupid").get(groupID))) {
-				//					continue;
-				//				}
-				System.out.println("nickname: " + nick);
-			} else
-				System.out.println("##########");
-
-			Matcher m1 = titlePattern.matcher(inner);
-			if (m1 != null && m1.find()) {
-				String title = m1.group(1);
-				title = title.replaceAll("<img .*?>", "").trim();
-				post.setTitle(title);
-				System.out.println("title: " + title.trim());
-			} else
-				System.out.println("##########");
-
-			String cont = "";
-			Matcher m2 = contentPattern.matcher(inner);
-			if (m2 != null && m2.find()) {
-				//					cont = m2.group(1).replaceAll("<.*?>", "").trim();
-				cont = m2.group(1).trim();
-				post.setContent(cont);
-				System.out.println("content: " + cont);
-			} else
-				System.out.println("##########");
-
-			String date = "";
-			Matcher m3 = datePattern.matcher(inner);
-			if (m3 != null && m3.find()) {
-				date = m3.group(1).replaceAll("<.*?>", "").trim();
-				System.out.println("date: " + date);
-				try {
-
-					post.setPtime(sdf.parse(date));
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} else
-				System.out.println("##########");
-
-			//				String filePath = Path + groupID + "//" + date.substring(0, 10) + ".txt";
-			//				System.out.println("filePath:" + filePath);
-			//				String key = groupID + "_" + date.substring(0, 10);
-			//				if (bdb.get(key) == null) {
-			//					bdb.put(key, cont);
+		//		if (m != null && m.find()) {
+		//			String inner = m.group(1);
+		//				System.out.println("inner: " + inner);
+		String inner = content;
+		Matcher m4 = nickPattern.matcher(inner);
+		if (m4 != null && m4.find()) {
+			String nick = m4.group(1).replaceAll("\\s", "").replaceAll("<.*?>", "").trim();
+			post.setNick(nick);
+			//				if (!nick.equals(config.getValue("groupid").get(groupID))) {
+			//					continue;
 			//				}
-			// 提取股票
-			Matcher stockm = stockPattern.matcher(cont);
-			// String stock="";
-			StringBuffer stock = new StringBuffer();
-			HashSet<String> st = new HashSet<String>();
-			while (stockm != null && stockm.find()) {
-				String tmp = stockm.group(1);
-				if (!st.contains(tmp)) {
-					stock.append(tmp + " ");
-					// System.out.println("stock: " + tmp);
-					st.add(tmp);
-				}
+			System.out.println("nickname: " + nick);
+		} else
+			System.out.println("##########");
+
+		Matcher m1 = titlePattern.matcher(inner);
+		if (m1 != null && m1.find()) {
+			String title = m1.group(1);
+			title = title.replaceAll("<img .*?>", "").trim();
+			post.setTitle(title);
+			System.out.println("title: " + title.trim());
+		} else
+			System.out.println("##########");
+
+		String cont = "";
+		Matcher m2 = contentPattern.matcher(inner);
+		if (m2 != null && m2.find()) {
+			//					cont = m2.group(1).replaceAll("<.*?>", "").trim();
+			cont = m2.group(1).trim();
+			post.setContent(cont);
+			System.out.println("content: " + cont);
+		} else
+			System.out.println("##########");
+
+		String date = "";
+		Matcher m3 = datePattern.matcher(inner);
+		if (m3 != null && m3.find()) {
+			date = m3.group(1).replaceAll("<.*?>", "").trim();
+			System.out.println("date: " + date);
+			try {
+
+				post.setPtime(sdf.parse(date));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			//				key += "_stock";
-			//				if (bdb.get(key) == null) {
-			//					bdb.put(key, stock.toString());
-			//				}
-			post.setRelatedstocks(stock.toString());
-			//				FileUtil.write(filePath, cont);
-			//				String stockPath = filePath.substring(0, filePath.indexOf('.')) + "_stock" + ".txt";
-			//				FileUtil.write(stockPath, stock.toString());
+		} else
+			System.out.println("##########");
 
+		//				String filePath = Path + groupID + "//" + date.substring(0, 10) + ".txt";
+		//				System.out.println("filePath:" + filePath);
+		//				String key = groupID + "_" + date.substring(0, 10);
+		//				if (bdb.get(key) == null) {
+		//					bdb.put(key, cont);
+		//				}
+		// 提取股票
+		Matcher stockm = stockPattern.matcher(cont);
+		// String stock="";
+		StringBuffer stock = new StringBuffer();
+		HashSet<String> st = new HashSet<String>();
+		while (stockm != null && stockm.find()) {
+			String tmp = stockm.group(1);
+			if (!st.contains(tmp)) {
+				stock.append(tmp + " ");
+				// System.out.println("stock: " + tmp);
+				st.add(tmp);
+			}
 		}
+		//				key += "_stock";
+		//				if (bdb.get(key) == null) {
+		//					bdb.put(key, stock.toString());
+		//				}
+		post.setRelatedstocks(stock.toString());
+		//				FileUtil.write(filePath, cont);
+		//				String stockPath = filePath.substring(0, filePath.indexOf('.')) + "_stock" + ".txt";
+		//				FileUtil.write(stockPath, stock.toString());
+
+		//		}
 		return post;
 	}
 
