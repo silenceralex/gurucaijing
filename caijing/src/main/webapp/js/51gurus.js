d1bf69f202c6b4557e85ce330e46d2933dd29767
ginuim@gmail.com <ginuim@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-16 20:28:34 +0000

    新用户注册
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1166 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/webapp/js/51gurus.js
+++ caijing/src/main/webapp/js/51gurus.js
@@ -166,6 +166,7 @@
       return { "x" : x, "y" : y + document.body.scrollTop };
    };
    cart = {
+      // 初始化购物车
       init : function ( user, orderId ) {
          var t = this;
          t.user = user;
@@ -183,6 +184,7 @@
             };
          });
       },
+      // 清除购物车
       clear : function () {
          var t = this;
          Rookie(function(){
@@ -190,7 +192,9 @@
             this.clear("orderId");
          });
       },
-      /* add : function ( id, title, type, num, price ) {
+      /* 
+      // 添加一个产品
+      add : function ( id, title, type, num, price ) {
          var t = this,
          isExist = false;
          var total = { num : 0, price : 0 };
@@ -212,7 +216,9 @@
             this.write( 'cart', t.cartArr );
          });
          t.popDialog( total.num, total.price );
-      }, */
+      }, 
+      */
+      // 添加一个产品
       add : function ( id, num, price ) {
          var t = this,
          isExist = false;
@@ -235,17 +241,51 @@
             this.write( 'cart', t.cartArr );
          });
          t.popDialog( total.num, total.price );
+         if ( $("#cartTb") ) {
+            $("#cartTb").append('\
+               <tr id="' + id + '">\
+                  <td>' + id + '</td>\
+                  <td>' + t.pObj[id].title + '</td>\
+                  <td>' + t.pObj[id].intro + '</td>\
+                  <td><span onclick="cart.sub(\'' + id + '\')">-</span><span id="' + id + 'num">' + num + '</span><span onclick="cart.plus(\'' + id + '\')">+</span></td>\
+                  <td><span class="price">' + price + '</span>元</td>\
+                  <td><a onclick="cart.del(' + id + ')" href="javascript:;">删除</a></td>\
+               </tr>\
+            ');
+         }
       },
+      // 删除某个产品
       del : function ( id ) {
          for ( var i = 0; i < t.cartArr; i ++ ) {
             if ( id == t.cartArr[i].id ) {
                t.cartArr.splice( i, 1 );
             }
          };
+         $( "#" + id ).remove(); // 删除该节点
          Rookie(function(){
             this.write( 'cart', t.cartArr );
          });
       },
+      // 产品个数加一
+      plus : function ( id ) {
+         var t = this,
+             num = Number($( "#" + id + "num" ).text());
+         num += 1;
+         $( "#" + id + "num" ).text( num );
+         t.motify( id, "num", num );
+      },
+      // 产品个数减一
+      sub : function ( id ) {
+         var t = this,
+             num = Number($( "#" + id + "num" ).text());
+         if( num <= 1 ) {
+            return;
+         }
+         num -= 1;
+         $( "#" + id + "num" ).text( num);
+         t.motify( id, "num", num );
+      },
+      // 修改数据
       motify : function ( id, field, value ) {
          for ( var i = 0; i < t.cartArr; i ++ ) {
             if ( id == t.cartArr[i].id ) {
@@ -256,17 +296,42 @@
             this.write( 'cart', t.cartArr );
          })
       },
+      // 展示数据
       show : function () {
+         /**
+          * pObj: {0002:{id:0002,title:"分析师成功率",intro:"套餐",price:10},0003:{...}}
+          */
+         var t = this,
+             cartTb = $("#cartTb"),
+             pid = "", // 产品id
+             num = 1, // 产品数量
+             price = 0, // 价格
+             totalN = 0, // 总数量
+             totalP = 0; // 总价格
          Rookie(function(){
-            console.log(this.read('cart'));
+            t.cartArr = this.read('cart');
          })
+         for ( var i = 0; i < t.cartArr.length; i++ ) {
+            pid = t.cartArr[i].id;
+            num = t.cartArr[i].num;
+            price = t.pObj[pid].price * num;
+            totalN += num;
+            totalP += price;
+            $( cartTb ).append('\
+               <tr id="' + pid + '">\
+                  <td>' + pid + '</td>\
+                  <td>' + t.pObj[pid].title + '</td>\
+                  <td>' + t.pObj[pid].intro + '</td>\
+                  <td><span onclick="cart.sub(\'' + pid + '\')">-</span><span id="' + pid + 'num">' + num + '</span><span onclick="cart.plus(\'' + pid + '\')">+</span></td>\
+                  <td><span class="price">' + price + '</span>元</td>\
+                  <td><a onclick="cart.del(' + pid + ')" href="javascript:;">删除</a></td>\
+               </tr>\
+            ');
+         };
+         $("#totalN").text( totalN );
+         $("#totalP").text( totalP );
       },
-      enter : function () {
-         
-      },
-      back : function () {
-         
-      },
+      // 弹出提示框
       popDialog : function ( num, price ) {
          var elem = $("#dialogS");
          $("#tipTotalN").innerHTML = num;
@@ -277,6 +342,102 @@
             left : ($(window).width() - $( elem ).outerWidth())/2,
             top : ($(window).height() - $( elem ).outerHeight())/2
          });
+      }
+   };
+   userInfo = {
+      nickReg : /^[\w_]{4,16}$/,
+      emailReg : /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+      mobileReg : /^1{1}\d{10}$/,
+      passReg : /^.{6,16}$/,
+      init : function () {
+         var t = this;
+      }
+   };
+   reg = {
+      nickReg : /^[\w_]{4,16}$/,
+      emailReg : /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+      passReg : /^.{6,16}$/,
+      emailNode : $("#email"),
+      pass1Node : $("#pass1"),
+      pass2Node : $("#pass2"),
+      nicknameNode : $("#nickname"),
+      emailTipNode : $("#emailTip"),
+      pass1TipNode : $("#pass1Tip"),
+      pass2TipNode : $("#pass2Tip"),
+      nicknameTipNode : $("#nicknameTip"),
+      // 初始化
+      init : function () {
+         var t = this;
+         // 绑定email验证事件
+         $( t.emailNode ).bind("blur", function () {
+            t.checkEmail();
+         });
+         $( t.emailNode ).bind("focus", function () {
+            $( t.emailTipNode ).fadeOut("slow");
+         });
+         // 绑定密码验证事件
+         $( t.pass1Node ).bind("blur", function () {
+            t.checkPass();
+         });
+         $( t.pass1Node ).bind("focus", function () {
+            $( t.pass1TipNode ).fadeOut("slow");
+         });
+         // 绑定密码验证事件
+         $( t.pass2Node ).bind("blur", function () {
+            t.checkPass2();
+         });
+         $( t.pass2Node ).bind("focus", function () {
+            $( t.pass2TipNode ).fadeOut("slow");
+         });
+         // 绑定昵称验证事件
+         $( t.nicknameNode ).bind("blur", function () {
+            t.checkNick();
+         });
+         $( t.nicknameNode ).bind("focus", function () {
+            $( t.nicknameTipNode ).fadeOut("slow");
+         });
+      },
+      // 通用检查表单
+      checkVal : function ( reg, node, tip ) {
+         var t = this,
+             val = $( node ).text();
+         
+         if ( val && reg.test( val ) ) {
+            $( tip ).fadeOut("slow");
+            return true;
+         } else {
+            $( tip ).fadeIn("slow");
+            return false;
+         };
+      },
+      // 验证邮件格式
+      checkEmail : function () {
+         t.checkVal( t.emailReg, $( t.emailNode ), $( t.emailTipNode ));
+      },
+      // 验证密码格式
+      checkPass : function () {
+         t.checkVal( t.passReg, $( t.pass1Node ), $( t.pass1TipNode ));
+      },
+      // 验证两次密码输入
+      checkPass2 : function () {
+         if ( t.pass1Node.text() != t.pass2Node.text() ) {
+            t.pass2TipNode.fadeIn("slow");
+            return false;
+         } else {
+            t.pass2TipNode.fadeOut("slow");
+            return true;
+         };
+      },
+      // 验证昵称
+      checkNick : function () {
+         t.checkVal( t.passReg, $( t.nicknameNode ), $( t.nicknameTipNode ));
+      },
+      // 验证表单并提交
+      formSubmit : function () {
+         var t = this;
+         if ( t.checkEmail && t.checkPass() &&t.checkPass2() && t.checkNick() ) {
+            document.reg.submit();
+         };
       }
    };
 })(window);
