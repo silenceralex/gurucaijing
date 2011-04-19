19d336b01c027da98d70fea05715aee43fd8054a
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-19 11:18:58 +0000

    添加produc、userright的dao
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1173 910414aa-d3c9-269b-9558-6cae95c76e5e

---
+++ caijing/src/main/java/com/caijing/dao/ibatis/ProductDAOImpl.java
@@ -0,0 +1,8 @@
+package com.caijing.dao.ibatis;
+
+import com.caijing.dao.ProductDAO;
+import com.caijing.util.CrudDaoDefault;
+
+public class ProductDAOImpl extends CrudDaoDefault implements ProductDAO {
+
+}
---
+++ caijing/src/main/java/com/caijing/dao/ibatis/UserrightDAOImpl.java
@@ -0,0 +1,8 @@
+package com.caijing.dao.ibatis;
+
+import com.caijing.dao.UserrightDAO;
+import com.caijing.util.CrudDaoDefault;
+
+public class UserrightDAOImpl extends CrudDaoDefault implements UserrightDAO {
+
+}
