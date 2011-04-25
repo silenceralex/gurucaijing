ac95a993feb562ff461b08ff3c37a6dadb587e54
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-25 09:36:25 +0000

    充值消费manager
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1209 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/business/OrderManager.java
+++ caijing/src/main/java/com/caijing/business/OrderManager.java
@@ -1,14 +1,29 @@
 package com.caijing.business;
 
+import java.util.List;
+
+import com.caijing.domain.Order;
+
 /**
  * 充值消费流程控制
+ * recharge插入 user余额更新 order消费插入 userright权限更新，session刷新
  */
 public interface OrderManager {
 
 	/**
-	 * 充值购买流程 recharge插入 user余额更新 order消费插入 userright权限更新，session刷新
+	 * recharge插入 user余额更新
 	 */
-	public boolean orderByRecharge();
+	public void handleRecharge(String userid, int type, float cash);
 	
-	
+	/**
+	 * order消费插入 userright权限更新
+	 */
+	void handleOrder(String userid, List<Order> orders);
+
+	/**
+	 * 非充值购买流程 recharge插入 user余额更新 order消费插入 userright权限更新，session刷新
+	 */
+//	boolean orderByRemain();
+//	public boolean orderByRecharge(String userid, int type, float cash);
+
 }
