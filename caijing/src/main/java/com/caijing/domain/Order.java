047ce7401b18b831052a2be989b6fff429866b44
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-25 02:52:12 +0000

    更新order domain
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1206 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/domain/Order.java
+++ caijing/src/main/java/com/caijing/domain/Order.java
@@ -1,5 +1,80 @@
 package com.caijing.domain;
 
+import java.util.Date;
+
 public class Order {
+
+	private Long orderid;
+	private Integer pid;
+	private Long userid;
+	private Integer num;
+	private Float cost;
+	private Byte status;
+	private Date ctime;
+	private Date lmodify;
+
+	public Long getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(Long orderid) {
+		this.orderid = orderid;
+	}
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+
+	public Float getCost() {
+		return cost;
+	}
+
+	public void setCost(Float cost) {
+		this.cost = cost;
+	}
+
+	public Byte getStatus() {
+		return status;
+	}
+
+	public void setStatus(Byte status) {
+		this.status = status;
+	}
+
+	public Date getCtime() {
+		return ctime;
+	}
+
+	public void setCtime(Date ctime) {
+		this.ctime = ctime;
+	}
+
+	public Date getLmodify() {
+		return lmodify;
+	}
+
+	public void setLmodify(Date lmodify) {
+		this.lmodify = lmodify;
+	}
 
 }
