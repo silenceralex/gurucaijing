ece76026f583a462b6851e9f64e41d407d3d1129
FJSllr <FJSllr@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-04-28 12:15:10 +0000

    Order
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1221 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/dao/OrderDao.java
+++ caijing/src/main/java/com/caijing/dao/OrderDao.java
@@ -1,7 +1,9 @@
 package com.caijing.dao;
 
+import com.caijing.domain.OrderPr;
 import com.caijing.util.CrudDao;
 
 public interface OrderDao extends CrudDao {
-
+	
+	public void insertOrderPr(OrderPr orderPr);
 }
--- caijing/src/main/java/com/caijing/dao/ibatis/OrderDaoImpl.java
+++ caijing/src/main/java/com/caijing/dao/ibatis/OrderDaoImpl.java
@@ -1,11 +1,17 @@
 package com.caijing.dao.ibatis;
 
 import com.caijing.dao.OrderDao;
+import com.caijing.domain.OrderPr;
 import com.caijing.util.CrudDaoDefault;
 
 public class OrderDaoImpl extends CrudDaoDefault implements OrderDao {
 	
 	public void selectWithOrderPr(){
 		
+	}
+
+	@Override
+	public void insertOrderPr(OrderPr orderPr) {
+		getSqlMapClientTemplate().insert(this.getNameSpace()+".insertOrderPr", orderPr);
 	}
 }
