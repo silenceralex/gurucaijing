d9a6c9501a948d5691cdd3daaf2005fd8afa28be
fjsllr@gmail.com <fjsllr@gmail.com@910414aa-d3c9-269b-9558-6cae95c76e5e>
2011-05-29 02:38:22 +0000

    20110523 20110526 研报分类处理
    git-svn-id: http://gurucaijing.googlecode.com/svn/trunk@1363 910414aa-d3c9-269b-9558-6cae95c76e5e

--- caijing/src/main/java/com/caijing/task/RenameReport2.java
+++ caijing/src/main/java/com/caijing/task/RenameReport2.java
@@ -87,10 +87,10 @@ public class RenameReport2 {
 			{
 //				put("/data/oldpapers/201008/201008.xls", "/data/oldpapers/201008/");
 //				put("/data/oldpapers/201008/yw201008_0411.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201008/yw201008_0418.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201008/yw201008_0418.xls", "/data/oldpapers/201008/");
 //				put("/data/oldpapers/201008/201008_0425.xls", "/data/oldpapers/201008/");
 //				put("/data/oldpapers/201009/201009.xlsx", "/data/oldpapers/201009/");
-				put("/data/oldpapers/201009/201009_0418.xlsx", "/data/oldpapers/201009/");
+//				put("/data/oldpapers/201009/201009_0418.xlsx", "/data/oldpapers/201009/");
 //				put("/data/oldpapers/201010/201010.xlsx", "/data/oldpapers/201010/");
 //				put("/data/oldpapers/201010/201010_0411.xlsx", "/data/oldpapers/201010/");
 //				put("/data/oldpapers/201010/201010_0425.xlsx", "/data/oldpapers/201010/");
@@ -99,18 +99,18 @@ public class RenameReport2 {
 //				put("/data/oldpapers/201011/201011_0425.xls", "/data/oldpapers/201011/");
 //				put("/data/oldpapers/201012/201012.xls", "/data/oldpapers/201012/");
 //				put("/data/oldpapers/201012/201012_0411.xls", "/data/oldpapers/201012/");
-				put("/data/oldpapers/201012/201012_0418.xls", "/data/oldpapers/201012/");
+//				put("/data/oldpapers/201012/201012_0418.xls", "/data/oldpapers/201012/");
 //				put("/data/oldpapers/201101temp/201101temp.xls", "/data/oldpapers/201101temp/");
-				put("/data/oldpapers/201102/201102_0418.xlsx", "/data/oldpapers/201102/");
+//				put("/data/oldpapers/201102/201102_0418.xlsx", "/data/oldpapers/201102/");
 //				put("/data/excel/hanjianping-1.xls", "/data/oldpapers2/hanjianping/");
 //				put("/data/excel/hanjianping-2.xls", "/data/oldpapers2/hanjianping/");
 //				put("/data/excel/hanjianping-3.xls", "/data/oldpapers2/hanjianping/");
-				put("/data/excel/hanjianping-4.xls", "/data/oldpapers2/hanjianping/");
+//				put("/data/excel/hanjianping-4.xls", "/data/oldpapers2/hanjianping/");
 //				put("/data/excel/hanjianping-5.xls", "/data/oldpapers2/hanjianping/"); //0425
 //				put("/data/excel/yanshiyou-1.xlsx", "/data/oldpapers2/yanshiyou/");
-				put("/data/excel/yanshiyou-2.xls", "/data/oldpapers2/yanshiyou/"); //0425
+//				put("/data/excel/yanshiyou-2.xls", "/data/oldpapers2/yanshiyou/"); //0425
 //				put("/data/excel/yanshiyou-3.xlsx", "/data/oldpapers2/yanshiyou/"); //0425
-				put("/data/excel/yanshiyou-4.xlsx", "/data/oldpapers2/yanshiyou/"); //0425 删除yanshiyou-4_bak.xlsx中的图表数据
+//				put("/data/excel/yanshiyou-4.xlsx", "/data/oldpapers2/yanshiyou/"); //0425 删除yanshiyou-4_bak.xlsx中的图表数据
 //				put("/data/excel/wanghan-1.xlsx", "/data/oldpapers2/wanghan/");
 //				put("/data/excel/wanghan-2.xlsx", "/data/oldpapers2/wanghan/");
 //				put("/data/excel/wanghan-3.xlsx", "/data/oldpapers2/wanghan/");
@@ -123,6 +123,15 @@ public class RenameReport2 {
 //				put("/data/excel/wanghan-5.xlsx", "/data/oldpapers2/wanghan/");
 //				put("/data/excel/yanshiyou-5.xlsx", "/data/oldpapers2/yanshiyou/");
 //				put("/data/excel/yanshiyou-6.xlsx", "/data/oldpapers2/yanshiyou/");
+				
+				//20100523
+				put("/data/oldpapers/201010/201010_0523.xls", "/data/oldpapers/201010/");
+				put("/data/oldpapers/201008/201008_0523.xls", "/data/oldpapers/201008/");
+				put("/data/excel/yanshiyou\\ 2011.5.23.xls", "/data/oldpapers2/yanshiyou/");
+				
+				//20100526
+				put("/data/oldpapers/201103/201103_0526.xls", "/data/oldpapers/201103/");
+				put("/data/excel/yanshiyou\\ 2011.5.26.xls", "/data/oldpapers2/yanshiyou/");
 			}
 		};
 		
@@ -144,44 +153,53 @@ public class RenameReport2 {
 		Map<String, String> map = new HashMap<String, String>() {
 			private static final long serialVersionUID = 1L;
 			{
-				put("/data/oldpapers/201008/201008.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201008/yw201008_0411.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201008/yw201008_0418.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201008/201008_0425.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201009/201009.xlsx", "/data/oldpapers/201009/");
-				put("/data/oldpapers/201009/201009_0418.xlsx", "/data/oldpapers/201009/");
-				put("/data/oldpapers/201010/201010.xlsx", "/data/oldpapers/201010/");
-				put("/data/oldpapers/201010/201010_0411.xlsx", "/data/oldpapers/201010/");
-				put("/data/oldpapers/201010/201010_0425.xlsx", "/data/oldpapers/201010/");
-				put("/data/oldpapers/201011/201011.xls", "/data/oldpapers/201011/");
-				put("/data/oldpapers/201011/201011_0411.xls", "/data/oldpapers/201011/");
-				put("/data/oldpapers/201011/201011_0425.xls", "/data/oldpapers/201011/");
-				put("/data/oldpapers/201012/201012.xls", "/data/oldpapers/201012/");
-				put("/data/oldpapers/201012/201012_0411.xls", "/data/oldpapers/201012/");
-				put("/data/oldpapers/201012/201012_0418.xls", "/data/oldpapers/201012/");
-				put("/data/oldpapers/201101temp/201101temp.xls", "/data/oldpapers/201101temp/");
-				put("/data/oldpapers/201102/201102_0418.xlsx", "/data/oldpapers/201102/");
-				put("/data/excel/hanjianping-1.xls", "/data/oldpapers2/hanjianping/");
-				put("/data/excel/hanjianping-2.xls", "/data/oldpapers2/hanjianping/");
-				put("/data/excel/hanjianping-3.xls", "/data/oldpapers2/hanjianping/");
-				put("/data/excel/hanjianping-4.xls", "/data/oldpapers2/hanjianping/");
-				put("/data/excel/hanjianping-5.xls", "/data/oldpapers2/hanjianping/"); //0425
-				put("/data/excel/yanshiyou-1.xlsx", "/data/oldpapers2/yanshiyou/");
-				put("/data/excel/yanshiyou-2.xls", "/data/oldpapers2/yanshiyou/"); //0425
-				put("/data/excel/yanshiyou-3.xlsx", "/data/oldpapers2/yanshiyou/"); //0425
-				put("/data/excel/yanshiyou-4.xlsx", "/data/oldpapers2/yanshiyou/"); //0425 删除yanshiyou-4_bak.xlsx中的图表数据
-				put("/data/excel/wanghan-1.xlsx", "/data/oldpapers2/wanghan/");
-				put("/data/excel/wanghan-2.xlsx", "/data/oldpapers2/wanghan/");
-				put("/data/excel/wanghan-3.xlsx", "/data/oldpapers2/wanghan/");
-				put("/data/excel/wanghan-4.xlsx", "/data/oldpapers2/wanghan/"); //0425
+//				put("/data/oldpapers/201008/201008.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201008/yw201008_0411.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201008/yw201008_0418.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201008/201008_0425.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201009/201009.xlsx", "/data/oldpapers/201009/");
+//				put("/data/oldpapers/201009/201009_0418.xlsx", "/data/oldpapers/201009/");
+//				put("/data/oldpapers/201010/201010.xlsx", "/data/oldpapers/201010/");
+//				put("/data/oldpapers/201010/201010_0411.xlsx", "/data/oldpapers/201010/");
+//				put("/data/oldpapers/201010/201010_0425.xlsx", "/data/oldpapers/201010/");
+//				put("/data/oldpapers/201011/201011.xls", "/data/oldpapers/201011/");
+//				put("/data/oldpapers/201011/201011_0411.xls", "/data/oldpapers/201011/");
+//				put("/data/oldpapers/201011/201011_0425.xls", "/data/oldpapers/201011/");
+//				put("/data/oldpapers/201012/201012.xls", "/data/oldpapers/201012/");
+//				put("/data/oldpapers/201012/201012_0411.xls", "/data/oldpapers/201012/");
+//				put("/data/oldpapers/201012/201012_0418.xls", "/data/oldpapers/201012/");
+//				put("/data/oldpapers/201101temp/201101temp.xls", "/data/oldpapers/201101temp/");
+//				put("/data/oldpapers/201102/201102_0418.xlsx", "/data/oldpapers/201102/");
+//				put("/data/excel/hanjianping-1.xls", "/data/oldpapers2/hanjianping/");
+//				put("/data/excel/hanjianping-2.xls", "/data/oldpapers2/hanjianping/");
+//				put("/data/excel/hanjianping-3.xls", "/data/oldpapers2/hanjianping/");
+//				put("/data/excel/hanjianping-4.xls", "/data/oldpapers2/hanjianping/");
+//				put("/data/excel/hanjianping-5.xls", "/data/oldpapers2/hanjianping/"); //0425
+//				put("/data/excel/yanshiyou-1.xlsx", "/data/oldpapers2/yanshiyou/");
+//				put("/data/excel/yanshiyou-2.xls", "/data/oldpapers2/yanshiyou/"); //0425
+//				put("/data/excel/yanshiyou-3.xlsx", "/data/oldpapers2/yanshiyou/"); //0425
+//				put("/data/excel/yanshiyou-4.xlsx", "/data/oldpapers2/yanshiyou/"); //0425 删除yanshiyou-4_bak.xlsx中的图表数据
+//				put("/data/excel/wanghan-1.xlsx", "/data/oldpapers2/wanghan/");
+//				put("/data/excel/wanghan-2.xlsx", "/data/oldpapers2/wanghan/");
+//				put("/data/excel/wanghan-3.xlsx", "/data/oldpapers2/wanghan/");
+//				put("/data/excel/wanghan-4.xlsx", "/data/oldpapers2/wanghan/"); //0425
 				
 				//20100513
-				put("/data/oldpapers/201008/201008_0513.xls", "/data/oldpapers/201008/");
-				put("/data/oldpapers/201102/201102_0513.xls", "/data/oldpapers/201102/");
+//				put("/data/oldpapers/201008/201008_0513.xls", "/data/oldpapers/201008/");
+//				put("/data/oldpapers/201102/201102_0513.xls", "/data/oldpapers/201102/");
 				//put("/data/excel/20110513.xls", "/data/oldpapers2/wanghan/"); //TODO
-				put("/data/excel/wanghan-5.xlsx", "/data/oldpapers2/wanghan/");
-				put("/data/excel/yanshiyou-5.xlsx", "/data/oldpapers2/yanshiyou/");
-				put("/data/excel/yanshiyou-6.xlsx", "/data/oldpapers2/yanshiyou/");
+//				put("/data/excel/wanghan-5.xlsx", "/data/oldpapers2/wanghan/");
+//				put("/data/excel/yanshiyou-5.xlsx", "/data/oldpapers2/yanshiyou/");
+//				put("/data/excel/yanshiyou-6.xlsx", "/data/oldpapers2/yanshiyou/");
+				
+				//20100523
+				put("/data/oldpapers/201010/201010_0523.xls", "/data/oldpapers/201010/");
+				put("/data/oldpapers/201008/201008_0523.xls", "/data/oldpapers/201008/");
+				put("/data/excel/yanshiyou\\ 2011.5.23.xls", "/data/oldpapers2/yanshiyou/");
+				
+				//20100526
+				put("/data/oldpapers/201103/201103_0526.xls", "/data/oldpapers/201103/");
+				put("/data/excel/yanshiyou\\ 2011.5.26.xls", "/data/oldpapers2/yanshiyou/");
 			}
 		};
 		
@@ -643,8 +661,8 @@ public class RenameReport2 {
 	
 	public static void main(String[] args) {
 		RenameReport2 rr = new RenameReport2();
-		rr.readExcels2();
-//		rr.readExcels2ForNum();
+//		rr.readExcels2();
+		rr.readExcels2ForNum();
 //		System.out.println(rr.getcreatedate("20080102"));
 //		System.out.println(rr.getcreatedate("200811207"));
 		System.exit(0);
